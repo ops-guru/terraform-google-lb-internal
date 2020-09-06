6247fe2d2cc8eebc6c95f6ca2361f5d02963c5c5
ViktorB <viktor@opsguru.io>
2020-09-07 00:05:03 +0300

    network region

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@
 data "google_compute_network" "network" {
   name    = var.network
   project = var.network_project == "" ? var.project : var.network_project
+  region  = var.region
 }
 
 data "google_compute_subnetwork" "network" {
