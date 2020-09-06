a89b5f38aadfd67e91c88fe6e0b23d8658280c96
ViktorB <viktor@opsguru.io>
2020-09-06 21:49:15 +0300

    forwarding rule network arg

--- main.tf
+++ main.tf
@@ -51,9 +51,7 @@ resource "google_compute_global_forwarding_rule" "default" {
   port_range            = "80"
   load_balancing_scheme = "INTERNAL_SELF_MANAGED"
   ip_address            = "0.0.0.0"
-  network {
-    network = data.google_compute_subnetwork.network.self_link
-  }
+  network               = data.google_compute_subnetwork.network.self_link
 }
 
 resource "google_compute_forwarding_rule" "default" {
